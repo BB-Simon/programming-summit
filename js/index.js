1869777337a536f6658df883639dc6037c565130
BB-Simon <simon.chowdery@gmail.com>
2023-01-06 17:42:03 +0200

    Add desktop and mobile version

---
+++ js/index.js
@@ -0,0 +1,56 @@
+const speakers = [
+  {
+    id: 1,
+    name: '',
+    title: 'Brekmen Professor of Entrepreneuial Studies at Harvard Low Schoo',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, 
+                alias! Ad quibusdam quam fugiat doloribus commodi enim 
+                sapiente corrupti quidem impedit, voluptatum, provident vitae at.`,
+    image: '',
+  },
+  {
+    id: 2,
+    name: '',
+    title: 'Director of Art Center Nabi and a board member of IPS Koria',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, 
+                alias! Ad quibusdam quam fugiat doloribus commodi enim 
+                sapiente corrupti quidem impedit, voluptatum, provident vitae at.`,
+    image: '',
+  },
+  {
+    id: 3,
+    name: '',
+    title: 'Executive Director of the Wikimedia Foundation',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, 
+                alias! Ad quibusdam quam fugiat doloribus commodi enim 
+                sapiente corrupti quidem impedit, voluptatum, provident vitae at.`,
+    image: '',
+  },
+  {
+    id: 3,
+    name: '',
+    title: 'Executive Director of the Wikimedia Foundation',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, 
+                alias! Ad quibusdam quam fugiat doloribus commodi enim 
+                sapiente corrupti quidem impedit, voluptatum, provident vitae at.`,
+    image: '',
+  },
+  {
+    id: 3,
+    name: '',
+    title: 'President of Young Pirates of Europe',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, 
+                alias! Ad quibusdam quam fugiat doloribus commodi enim 
+                sapiente corrupti quidem impedit, voluptatum, provident vitae at.`,
+    image: '',
+  },
+  {
+    id: 3,
+    name: '',
+    title: 'CEO of Creative Commons. ex COO of the Mozilla Foundation',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, 
+                alias! Ad quibusdam quam fugiat doloribus commodi enim 
+                sapiente corrupti quidem impedit, voluptatum, provident vitae at.`,
+    image: '',
+  },
+];
